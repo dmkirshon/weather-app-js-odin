0a9ae2a39099a1056ade9324cd8f008ce632e882
dmkirshon <kirshon.david@gmail.com>
2022-03-03 11:51:54 +0200

    chores: Set up environment using web-build-env doc

---
+++ webpack.config.js
@@ -0,0 +1,26 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  mode: "development",
+  entry: "./src/index.js",
+  output: {
+    filename: "bundle.js",
+    path: path.resolve(__dirname, "dist"),
+    assetModuleFilename: "images/[hash][ext][query]",
+    clean: true,
+  },
+  plugins: [new HtmlWebpackPlugin({ title: "Weather App" })],
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: "asset/resource",
+      },
+    ],
+  },
+};
